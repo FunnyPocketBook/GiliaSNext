6fd98409a923a6ce47d4ec68fcf42a0c4244989c
FunnyPocketBook <ying-kai.dang@outlook.com>
2020-11-11 02:09:14 +0100

    Attempt no. 2 at cookies

--- GiliaSNext/GiliaSNext.cs
+++ GiliaSNext/GiliaSNext.cs
@@ -12,11 +12,16 @@ namespace GiliaSNext
     class GiliaSNext
     {
         private static ConfigBuilder builder = ConfigBuilder.Instance();
-        private static CookieContainer cookieJar = new CookieContainer();
+        private static HttpClientHandler handler = new HttpClientHandler();
+        private static HttpClient client = new HttpClient(handler);
         private static Uri iliasLoginUrl;
+        private static readonly Uri ILIAS_URI = new Uri("https://ilias.uni-konstanz.de/");
+
         static void Main(string[] args)
         {
             builder.Load();
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36");
+            client.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
             GetLoginLink().Wait();
             IliasLogin().Wait();
             TestLogin().Wait();
@@ -24,18 +29,13 @@ namespace GiliaSNext
 
         static async Task<int> IliasLogin()
         {
-            HttpClientHandler handler = new HttpClientHandler();
-            HttpClient client = new HttpClient(handler);
-            handler.AllowAutoRedirect = false;
-            handler.CookieContainer = cookieJar;
-            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36");
-            client.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
             Dictionary<string, string> postParamsDict = new Dictionary<string, string>
             {
                 { "username", builder.Config.User },
                 { "password", builder.Config.PasswordIlias }
             };
             FormUrlEncodedContent postParams = new FormUrlEncodedContent(postParamsDict);
+
             Console.WriteLine("Logging in...");
             try
             {
@@ -46,10 +46,6 @@ namespace GiliaSNext
                     return -1;
                 }
                 Console.WriteLine("Logged in");
-                string pageContents = await response.Content.ReadAsStringAsync();
-                HtmlDocument pageDocument = new HtmlDocument();
-                pageDocument.LoadHtml(pageContents);
-                //Console.WriteLine(pageDocument.DocumentNode.InnerText);
                 return 0;
             }
             catch (TaskCanceledException e)
@@ -65,16 +61,11 @@ namespace GiliaSNext
             Console.WriteLine("Test");
             try
             {
-                HttpClientHandler handler = new HttpClientHandler();
-                HttpClient client = new HttpClient(handler);
-                handler.CookieContainer = cookieJar;
-                Helper.Helper.PrintCookies(cookieJar, iliasLoginUrl);
-                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36");
-                HttpResponseMessage response = await client.GetAsync("https://ilias.uni-konstanz.de/ilias/ilias.php?baseClass=ilPersonalDesktopGUI&cmd=jumpToSelectedItems");
+                HttpResponseMessage response = await client.GetAsync("https://ilias.uni-konstanz.de/ilias/goto_ilias_uni_file_1082633_download.html");
                 string pageContents = await response.Content.ReadAsStringAsync();
                 HtmlDocument pageDocument = new HtmlDocument();
                 pageDocument.LoadHtml(pageContents);
-                //Console.WriteLine(pageDocument.DocumentNode.InnerText);
+                Console.WriteLine(pageDocument.DocumentNode.InnerText);
                 return 0;
             }
             catch (TaskCanceledException e)
@@ -107,5 +98,31 @@ namespace GiliaSNext
                 return -1;
             }
         }
+        public static CookieContainer ReadCookies(HttpResponseMessage response)
+        {
+            Uri pageUri = response.RequestMessage.RequestUri;
+            var cookieContainer = new CookieContainer();
+            IEnumerable<string> cookies;
+            if (response.Headers.TryGetValues("set-cookie", out cookies))
+            {
+                int counter = 0;
+                foreach (var c in cookies)
+                {
+                    if (counter == 2)
+                    {
+                        break;
+                    }
+                    cookieContainer.SetCookies(ILIAS_URI, c.Replace("/ilias/", "/"));
+                    counter++;
+                    /*var tempContainer = new CookieContainer();
+                    tempContainer.SetCookies(ILIAS_URI, c.Replace("/ilias/", "/"));
+                    CookieCollection cColl = tempContainer.GetCookies(ILIAS_URI);
+                    cookieContainer.Add(cColl[0]);*/
+                }
+            }
+            Cookies.PrintCookies(cookieContainer);
+            return cookieContainer;
+        }
+
     }
 }
--- GiliaSNext/Helper/Helper.cs
+++ GiliaSNext/Helper/Helper.cs
@@ -1,16 +1,32 @@
 ﻿using System;
+using System.Collections;
 using System.Net;
+using System.Reflection;
 
 namespace GiliaSNext.Helper
 {
-    class Helper
+    class Cookies
     {
-        public static void PrintCookies(CookieContainer cookieJar, Uri uri)
+        public static void PrintCookies(CookieContainer cookieJar)
         {
-            foreach (Cookie cookie in cookieJar.GetCookies(uri))
+            Hashtable table = (Hashtable)cookieJar.GetType().InvokeMember(
+                "m_domainTable",
+                BindingFlags.NonPublic |
+                BindingFlags.GetField |
+                BindingFlags.Instance,
+                null,
+                cookieJar,
+                new object[] { }
+            );
+            foreach (var key in table.Keys)
             {
-                Console.WriteLine("Name = {0} ; Value = {1} ; Domain = {2}",
-                    cookie.Name, cookie.Value, cookie.Domain);
+                Uri uri = new Uri(string.Format("https://{0}/", key));
+
+                foreach (Cookie cookie in cookieJar.GetCookies(uri))
+                {
+                    Console.WriteLine("Name = {0} ; Value = {1} ; Domain = {2}",
+                        cookie.Name, cookie.Value, cookie.Domain);
+                }
             }
         }
     }
