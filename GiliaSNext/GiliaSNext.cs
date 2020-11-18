1545d0e373e67fbfbd660656ce2b5c5d7c6df9d3
FunnyPocketBook <ying-kai.dang@outlook.com>
2020-11-18 23:59:45 +0100

    Added correct parameter to login Thanks Lennard!

--- GiliaSNext/GiliaSNext.cs
+++ GiliaSNext/GiliaSNext.cs
@@ -12,19 +12,18 @@ namespace GiliaSNext
     class GiliaSNext
     {
         private static ConfigBuilder builder = ConfigBuilder.Instance();
-        private static HttpClientHandler handler = new HttpClientHandler();
-        private static HttpClient client = new HttpClient(handler);
+        private static HttpClientHandler httpHandler = new HttpClientHandler();
+        private static HttpClient client = new HttpClient(httpHandler);
         private static Uri iliasLoginUrl;
-        private static readonly Uri ILIAS_URI = new Uri("https://ilias.uni-konstanz.de/");
 
         static void Main(string[] args)
         {
             builder.Load();
+            httpHandler.AllowAutoRedirect = true;
             client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36");
             client.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
             GetLoginLink().Wait();
             IliasLogin().Wait();
-            TestLogin().Wait();
         }
 
         static async Task<int> IliasLogin()
@@ -32,7 +31,8 @@ namespace GiliaSNext
             Dictionary<string, string> postParamsDict = new Dictionary<string, string>
             {
                 { "username", builder.Config.User },
-                { "password", builder.Config.PasswordIlias }
+                { "password", builder.Config.PasswordIlias },
+                { "cmd[doStandardAuthentication]", "Anmelden" }
             };
             FormUrlEncodedContent postParams = new FormUrlEncodedContent(postParamsDict);
 
@@ -46,22 +46,6 @@ namespace GiliaSNext
                     return -1;
                 }
                 Console.WriteLine("Logged in");
-                return 0;
-            }
-            catch (TaskCanceledException e)
-            {
-                Console.WriteLine("[Ilias] Login timed out.");
-                Console.WriteLine(e.InnerException.Message);
-                return -1;
-            }
-        }
-
-        static async Task<int> TestLogin()
-        {
-            Console.WriteLine("Test");
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync("https://ilias.uni-konstanz.de/ilias/goto_ilias_uni_file_1082633_download.html");
                 string pageContents = await response.Content.ReadAsStringAsync();
                 HtmlDocument pageDocument = new HtmlDocument();
                 pageDocument.LoadHtml(pageContents);
@@ -70,7 +54,7 @@ namespace GiliaSNext
             }
             catch (TaskCanceledException e)
             {
-                Console.WriteLine("[Ilias] Fetching login URL timed out.");
+                Console.WriteLine("[Ilias] Login timed out.");
                 Console.WriteLine(e.InnerException.Message);
                 return -1;
             }
@@ -87,7 +71,7 @@ namespace GiliaSNext
                 HtmlDocument pageDocument = new HtmlDocument();
                 pageDocument.LoadHtml(pageContents);
                 HtmlNode form = pageDocument.DocumentNode.SelectSingleNode("//form[@id='form_']");
-                iliasLoginUrl = new Uri("https://ilias.uni-konstanz.de/ilias/" + WebUtility.HtmlDecode(form.Attributes["action"].Value));
+                iliasLoginUrl = new Uri("https://ilias.uni-konstanz.de/ilias/" + WebUtility.HtmlDecode(form.Attributes["action"].Value) + "&client_id=ilias_uni");
                 Console.WriteLine(iliasLoginUrl);
                 return 0;
             }
@@ -97,31 +81,6 @@ namespace GiliaSNext
                 Console.WriteLine(e.InnerException.Message);
                 return -1;
             }
-        }
-        public static CookieContainer ReadCookies(HttpResponseMessage response)
-        {
-            Uri pageUri = response.RequestMessage.RequestUri;
-            var cookieContainer = new CookieContainer();
-            IEnumerable<string> cookies;
-            if (response.Headers.TryGetValues("set-cookie", out cookies))
-            {
-                int counter = 0;
-                foreach (var c in cookies)
-                {
-                    if (counter == 2)
-                    {
-                        break;
-                    }
-                    cookieContainer.SetCookies(ILIAS_URI, c.Replace("/ilias/", "/"));
-                    counter++;
-                    /*var tempContainer = new CookieContainer();
-                    tempContainer.SetCookies(ILIAS_URI, c.Replace("/ilias/", "/"));
-                    CookieCollection cColl = tempContainer.GetCookies(ILIAS_URI);
-                    cookieContainer.Add(cColl[0]);*/
-                }
-            }
-            Cookies.PrintCookies(cookieContainer);
-            return cookieContainer;
         }
 
     }
