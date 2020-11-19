65d1d57b80117d3b3db1fb4e4b3c79641f8cefa2
FunnyPocketBook <ying-kai.dang@outlook.com>
2020-11-19 03:02:52 +0100

    Split code and added methods GetRssFeed and DownloadFile

--- GiliaSNext/Config/Config.cs
+++ GiliaSNext/Config/Config.cs
@@ -7,5 +7,6 @@ namespace GiliaSNext.Config
         public string User { get; set; } = "first.last";
         public string PasswordIlias { get; set; } = "hunter12";
         public string PasswordRss { get; set; } = "hunter12";
+        public string RssUrl { get; set; } = "https://first.last:-password-@ilias.uni-konstanz.de/ilias/privfeed.php?client_id=ilias_uni&user_id=000000&hash=0123456789abcdefghjklmnopqrst";
     }
 }
--- GiliaSNext/GiliaSNext.cs
+++ GiliaSNext/GiliaSNext.cs
@@ -4,84 +4,27 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using GiliaSNext.Config;
-using GiliaSNext.Helper;
 using HtmlAgilityPack;
 
 namespace GiliaSNext
 {
     class GiliaSNext
     {
-        private static ConfigBuilder builder = ConfigBuilder.Instance();
-        private static HttpClientHandler httpHandler = new HttpClientHandler();
-        private static HttpClient client = new HttpClient(httpHandler);
-        private static Uri iliasLoginUrl;
+        private static ConfigBuilder Builder = ConfigBuilder.Instance();
+        private static Ilias Ilias;
 
         static void Main(string[] args)
         {
-            builder.Load();
-            httpHandler.AllowAutoRedirect = true;
-            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36");
-            client.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
-            GetLoginLink().Wait();
-            IliasLogin().Wait();
+            Builder.Load();
+            Ilias = new Ilias(Builder.Config.User, Builder.Config.PasswordIlias);
+            Test().Wait();
         }
 
-        static async Task<int> IliasLogin()
+        static async Task Test()
         {
-            Dictionary<string, string> postParamsDict = new Dictionary<string, string>
-            {
-                { "username", builder.Config.User },
-                { "password", builder.Config.PasswordIlias },
-                { "cmd[doStandardAuthentication]", "Anmelden" }
-            };
-            FormUrlEncodedContent postParams = new FormUrlEncodedContent(postParamsDict);
-
-            Console.WriteLine("Logging in...");
-            try
-            {
-                HttpResponseMessage response = await client.PostAsync(iliasLoginUrl, postParams);
-                if (response.StatusCode.CompareTo(HttpStatusCode.OK) != 0)
-                {
-                    Console.WriteLine("[Ilias] Status Code {0}", response.StatusCode);
-                    return -1;
-                }
-                Console.WriteLine("Logged in");
-                string pageContents = await response.Content.ReadAsStringAsync();
-                HtmlDocument pageDocument = new HtmlDocument();
-                pageDocument.LoadHtml(pageContents);
-                Console.WriteLine(pageDocument.DocumentNode.InnerText);
-                return 0;
-            }
-            catch (TaskCanceledException e)
-            {
-                Console.WriteLine("[Ilias] Login timed out.");
-                Console.WriteLine(e.InnerException.Message);
-                return -1;
-            }
+            await Ilias.Login();
+            await Ilias.GetRssFeed(Builder.Config.RssUrl, Builder.Config.User, Builder.Config.PasswordRss);
+            await Ilias.DownloadFile("1126607", "C:\\Users\\dangy\\Desktop\\bla.pdf");
         }
-
-        static async Task<int> GetLoginLink()
-        {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36");
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync("https://ilias.uni-konstanz.de/ilias/login.php");
-                string pageContents = await response.Content.ReadAsStringAsync();
-                HtmlDocument pageDocument = new HtmlDocument();
-                pageDocument.LoadHtml(pageContents);
-                HtmlNode form = pageDocument.DocumentNode.SelectSingleNode("//form[@id='form_']");
-                iliasLoginUrl = new Uri("https://ilias.uni-konstanz.de/ilias/" + WebUtility.HtmlDecode(form.Attributes["action"].Value) + "&client_id=ilias_uni");
-                Console.WriteLine(iliasLoginUrl);
-                return 0;
-            }
-            catch (TaskCanceledException e)
-            {
-                Console.WriteLine("[Ilias] Fetching login URL timed out.");
-                Console.WriteLine(e.InnerException.Message);
-                return -1;
-            }
-        }
-
     }
 }
